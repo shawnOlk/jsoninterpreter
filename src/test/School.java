3fe99b5ea3aa650decaada98e13e2d76bb061cdd
sunlei <365243317@qq.com>
2017-12-17 22:57:05 +0800

    666

--- src/test/School.java
+++ src/test/School.java
@@ -11,7 +11,7 @@ public class School {
     private String address;
     private List<Student> students;
 
-    private int i=35;
+    private int i=95;
 
     public School() {
         this.name = "shawn";
